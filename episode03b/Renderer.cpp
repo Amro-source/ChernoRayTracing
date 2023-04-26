52ac2e28445bb0f21236d759c06eaf12c25afea6
Amro <68817735+Amro-source@users.noreply.github.com>
2023-04-26 23:22:15 +0200

    Add files via upload

---
+++ episode03b/Renderer.cpp
@@ -0,0 +1,83 @@
+#include "Renderer.h"
+
+#include "Walnut/Random.h"
+
+#include <execution>
+#include <glm/common.hpp>
+
+
+void Renderer::OnResize(uint32_t width, uint32_t height)
+{
+	
+	if (m_FinalImage)
+	{
+		//no resize necessary 
+		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
+			return;
+		
+		m_FinalImage->Resize(width, height);
+
+	}
+	else
+	{
+		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+	}
+	
+	
+	delete[] m_ImageData;
+	m_ImageData = new uint32_t[width *height];
+
+
+
+}
+
+
+void Renderer::Render()
+{
+
+
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
+	{
+		
+		for (uint32_t x = 0; x < m_FinalImage->GetWidth() ; x++)
+		{
+			glm::vec2 coord = { (float)x /(float) m_FinalImage->GetWidth() ,(float) y /(float) m_FinalImage->GetHeight() };
+			m_ImageData[x+y* m_FinalImage->GetWidth()]=PerPixel(coord);
+
+
+		}
+
+	}
+
+
+	m_FinalImage->SetData(m_ImageData);
+
+
+
+}
+
+uint32_t Renderer::PerPixel(glm::vec2 coord)
+{
+	uint8_t r = (uint8_t)(coord.x * 255.0f);
+	uint8_t g = (uint8_t)(coord.y * 255.0f);
+
+	return 0xff000000 | (g<<8) | r;
+	
+	//return 0xffff00ff;
+
+
+
+
+}
+
+uint32_t PerPixel(glm::vec2 coord)
+{
+
+	return  0xffff00ff ;
+
+
+
+
+
+
+}
